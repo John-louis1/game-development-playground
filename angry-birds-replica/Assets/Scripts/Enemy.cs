3f09ed73becd1bbcd1375e05da621d71902719b0
unknown <john.flouis1@gmail.com>
2022-09-02 19:30:11 +0200

    Add enemy prefab and die function

---
+++ angry-birds-replica/Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 4f;
+    void OnCollisionEnter2D(Collision2D colInfo) {
+
+        if (colInfo.relativeVelocity.magnitude > health) {
+            Die();
+        }
+    }
+    void Die() {
+        Destroy(gameObject);
+    }
+}
