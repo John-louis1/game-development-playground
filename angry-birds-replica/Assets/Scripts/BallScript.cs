ae9b21566a8b9b91e86d2bd8be81563bef63b095
unknown <john.flouis1@gmail.com>
2022-09-02 21:37:51 +0200

    Add ball, enemy and enemy death effect prefabs
    Finish level 1 scene

--- angry-birds-replica/Assets/Scripts/BallScript.cs
+++ angry-birds-replica/Assets/Scripts/BallScript.cs
@@ -1,34 +1,50 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallScript : MonoBehaviour {
-    public Rigidbody2D rb;
+    public Rigidbody2D rbBall;
+    public Rigidbody2D rbHook;
+    public float maxDragDistance = 2f;
     public float releaseTime = .15f;
     private bool isPressed = false;
+    public GameObject nextBall;
     void Start() {
         
     }
 
     void Update() {
         if (isPressed) {
-            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos, rbHook.position) > maxDragDistance) {
+			rbBall.position = rbHook.position + (mousePos - rbHook.position).normalized * maxDragDistance;
+            } else {
+                rbBall.position = mousePos;
+            }
         }
     }
 
     void OnMouseDown() {
         isPressed = true;
-        rb.isKinematic = true;
+        rbBall.isKinematic = true;
     }
 
     void OnMouseUp() {
         isPressed = false;
-        rb.isKinematic = false; 
+        rbBall.isKinematic = false; 
         StartCoroutine(Release());
     }
 
     IEnumerator Release() {
         yield return new WaitForSeconds(releaseTime);
         GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+        yield return new WaitForSeconds(2f);
+        if (nextBall != null) {
+            nextBall.SetActive(true);
+        } else {
+            Enemy.EnemiesAlive = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
--- angry-birds-replica/Assets/Scripts/Enemy.cs
+++ angry-birds-replica/Assets/Scripts/Enemy.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public static int EnemiesAlive = 0;
+    public GameObject deathEffect;
     public float health = 4f;
+    void Start() {
+        EnemiesAlive++;
+    }
     void OnCollisionEnter2D(Collision2D colInfo) {
-
         if (colInfo.relativeVelocity.magnitude > health) {
             Die();
         }
     }
+    
     void Die() {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0) {
+            Debug.Log("Level WON!");
+        }
     }
 }
