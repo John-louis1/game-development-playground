94df0d116434aa36d6ed5f8d8226f479b684e148
unknown <john.flouis1@gmail.com>
2022-09-02 12:22:23 +0200

    Create basic main level scene

--- click-and-destroy/Assets/Scripts/ScriptPlayer.cs
+++ click-and-destroy/Assets/Scripts/ScriptPlayer.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class ScriptPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        // Use mouse to select a game object in the scene
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+
+        }
     }
 }
